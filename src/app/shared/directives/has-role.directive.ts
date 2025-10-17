f6df4c50cc218571512b9941bda0c071af58a4c9
FaheemCHAB <mdfaheem.aitrich@gmail.com>
2025-10-17 16:04:29 +0530

    feat: consultant added

--- src/app/shared/directives/has-role.directive.ts
+++ src/app/shared/directives/has-role.directive.ts
@@ -5,34 +5,42 @@ import { AuthService, UserRole } from '../../auth/services/auth.service';
   selector: '[appHasRole]'
 })
 export class HasRoleDirective {
-
-    private roles: UserRole[] = [];
+  private roles: string[] = [];
+  private hasView = false;
 
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef,
     private authService: AuthService
   ) {
-    // Use effect to react to role changes (Angular 19 approach)
     effect(() => {
       this.updateView();
     });
   }
 
   @Input()
-  set appHasRole(roles: UserRole | UserRole[]) {
-    this.roles = Array.isArray(roles) ? roles : [roles];
+  set appHasRole(roles: UserRole | UserRole[] | string | string[]) {
+    // Normalize input to array of strings
+    if (typeof roles === 'string') {
+      this.roles = [roles];
+    } else if (Array.isArray(roles)) {
+      this.roles = roles.map(r => String(r));
+    } else {
+      this.roles = [String(roles)];
+    }
     this.updateView();
   }
 
   private updateView(): void {
     const hasRole = this.authService.hasAnyRole(this.roles);
     
-    if (hasRole) {
-      this.viewContainer.createEmbeddedView(this.templateRef);
-    } else {
+    if (hasRole && !this.hasView) {
       this.viewContainer.clear();
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.hasView = true;
+    } else if (!hasRole && this.hasView) {
+      this.viewContainer.clear();
+      this.hasView = false;
     }
   }
-
 }
